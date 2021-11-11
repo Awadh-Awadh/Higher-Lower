79eac0d5ef131bb320fef7eecc68c6bd547d5fb1
Awadh-Awadh <awadhawadh84@yahoo.com>
2021-11-11 10:48:18 +0300

    refactor code

--- main.py
+++ main.py
@@ -3,16 +3,29 @@ from art import  *
 from random import choice
 
 
+def compare(value_a, value_b):
+   """Check if user is correct"""
+#Display art
+print(logo)
+
+#generate a random account from the game data
 choice_one = choice(data)
 choice_two = choice(data)
+if choice_two == choice_one:
+  choice_two = choice(data)
+
+#Format choices into printable data
+print(f"Compare A: {choice_one['name']}, {choice_one['description']}, from {choice_one['country']}")
+print(vs)
+print(f"Against B: {choice_two['name']}, {choice_two['description']}, from {choice_two['country']}")
+
+
+#Ask user for a guess
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+#check user if is correct
 value_A = choice_one['follower_count']
 value_B = choice_two['follower_count']
+ 
 
-def compare(value_a, value_b):
-  ...
-
-
-
-
-print(f"Compare A: {choice_one['name']}, {choice_one['description']}, from {choice_one['country']}")
-print(f"Compare A: {choice_two['name']}, {choice_two['description']}, from {choice_two['country']}")
+   
